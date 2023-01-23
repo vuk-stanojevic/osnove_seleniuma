24c21b6c9b7615ce3e5bf3f658f5e4e0b6251f11
Vuk Stanojevic <vukstanojevic42@gmail.com>
2023-01-23 22:01:13 +0100

    Chrome Driver setup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
     }
 }

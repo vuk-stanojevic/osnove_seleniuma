10f233ca58759f3f1d5215606cda6df453b32060
Vuk Stanojevic <vukstanojevic42@gmail.com>
2023-01-30 18:14:24 +0100

    Domaci 27.1.2023.

--- src/test/java/domaci_27_1_2023/Zadatak1.java
+++ src/test/java/domaci_27_1_2023/Zadatak1.java
@@ -1,8 +1,13 @@
 package domaci_27_1_2023;
 
+import helper.Helper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
 
 public class Zadatak1 {
     public static void main(String[] args) throws InterruptedException {
@@ -15,13 +20,19 @@ public class Zadatak1 {
 
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://mdbootstrap.com/docs/standard/components/toasts/#section-basic-example");
-        driver.findElement(By.linkText("//button[text()='Primary']")).click();
-
-
+        List<WebElement> buttons = driver.findElements(By.xpath("//section[@class='p-4 d-flex justify-content-center text-center w-100']" +
+                "//div[@class='container text-center']/button"));
+        for (int i = 0; i < buttons.size(); i++) {
+            buttons.get(i).click();
+            Thread.sleep(1000);
+            new Helper().elementExists(driver, By.xpath("//div[contains(@id, 'basic')]"));
+            Thread.sleep(1000);
+        }
         Thread.sleep(5000);
         driver.quit();
-
     }
 }
